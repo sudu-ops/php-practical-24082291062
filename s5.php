ea31ccfd56340abe967bb15923cd503d61fd76dd
sudu-ops <sragraval2005@gmail.com>
2026-02-26 22:37:22 +0530

    Create s5.php

---
+++ s5.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "write a PHP script using a for loop to generate number patterns or multiplication tables. <br>";
+    echo "Name : Sudarshan Agraval <br>";
+    echo "Enrollment Number : 24082291062 <br>";
+
+    $num = 5;
+
+    for ($i=1; $i<=10; $i++){
+        echo "$num X $i=".($num * $i)."<br>";
+    }
+
+?>
