a326acff7198a606daeb8cc93ad2d1ac07ee2446
sudu-ops <sragraval2005@gmail.com>
2026-02-26 22:43:29 +0530

    Create s7.php

---
+++ s7.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "Write a PHP script to demonstrate user-defined functions with parameters and return values. <br>";
+    echo "Name : Sudarshan Agraval <br>";
+    echo "Enrollment Number : 24082291062 <br>";
+
+    function add ($a, $b) {
+        return $a + $b;
+    }
+
+    echo "sum=" .add(10 , 20); 
+
+?>
