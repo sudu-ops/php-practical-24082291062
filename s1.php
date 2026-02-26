9cb2619a1a5f66c35c65cc61ef5add04b76c4183
sudu-ops <sragraval2005@gmail.com>
2026-02-26 22:27:56 +0530

    Add PHP script to display message and variables

---
+++ s1.php
@@ -0,0 +1,19 @@
+<?php
+    echo "Write a PHP script to display a simple message using echo/print and demonstrate variables and data types using the var_dump() function. <br>";
+    echo "Name : Sudarshan Agraval <br>";
+    echo "Enrollment Number : 24082291062 <br>";
+
+
+    echo "Welcome to PHP!<br>";
+
+    $name ="Sudarshan";
+    $age=20;
+    $price =99.5;
+    $isstudent =True;
+
+    var_dump($name);
+    var_dump($age);
+    var_dump($price);
+    var_dump($isstudent);
+
+?>
