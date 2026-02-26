5c04ac25962347207de743603734f436e2fd2d1f
sudu-ops <sragraval2005@gmail.com>
2026-02-26 22:45:20 +0530

    Create s8.php

---
+++ s8.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "Write a PHP script to demonstrate basic string functions such as strlen(), substr(), trim(), and strpos(). <br>";
+    echo "Name : Sudarshan Agraval <br>";
+    echo "Enrollment Number : 24082291062 <br>";
+
+    $str = "Hello PHP";
+
+    echo strlen($str) ."<br>";
+    echo substr($str, 1, 5)."<br>";
+    echo trim($str)."<br>";
+    echo strpos($str, "PHP");
+
+?>
