cb68486ad2c0a2f739bf22d1e5ab295d21e441f4
sudu-ops <sragraval2005@gmail.com>
2026-02-26 22:46:20 +0530

    Add PHP script for indexed arrays and operations

---
+++ s9.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "Write a PHP script to create and display indexed arrays and perform basic array operations. <br>";
+    echo "Name : Sudarshan Agraval <br>";
+    echo "Enrollment Number : 24082291062 <br>";
+
+    $colors = array("Red", "Green", "Blue");
+    array_push($colors, "Yellow");
+
+    foreach ($colors as $e) {
+        echo $e."<br>";
+    }
+
+?>
