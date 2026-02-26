2a4a9183026da598f5d6a192db062660842a5147
sudu-ops <sragraval2005@gmail.com>
2026-02-26 22:30:10 +0530

    Add PHP script demonstrating constants and operators

---
+++ s2.php
@@ -0,0 +1,16 @@
+<?php
+    echo "Write a PHP script to demonstrate the use of constants and arithmetic/logical operators in PHP. <br>";
+    echo "Name : Sudarshan Agraval<br>";
+    echo "Enrollment Number : 24082291062 <br>";
+
+    define ("prac1",3.14);
+
+    $a = 10;
+    $b = 5;
+
+    echo "prac1=" .prac1. "<br>";
+    echo "Addition=" .($a + $b). "<br>";
+    echo "Multiplication=" .($a * $b). "<br>";
+    echo "Logical And=" .($a > 5 && $b < 10);
+
+?>
